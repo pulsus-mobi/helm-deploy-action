e1679e086e996a50a48f5a80d760409fb455068c
Fabio Silva <fabio.silva@pulsus.mobi>
2025-04-14 16:15:20 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function run() {
   try {
@@ -8,6 +10,11 @@ async function run() {
     const imageTag = core.getInput('image_tag');
     const chart = core.getInput('chart'); // Recebe o caminho do chart
 
+    // Verifica se o chart existe
+    if (!fs.existsSync(chart)) {
+      throw new Error(`O chart '${chart}' não foi encontrado!`);
+    }
+
     core.info(`Deploying ${appName} using Helm`);
 
     // Comando para instalar ou atualizar o release usando Helm
@@ -15,7 +22,7 @@ async function run() {
 
     // Executa o comando Helm
     await exec.exec(helmCommand);
-    
+
     core.info('Deploy concluído com sucesso!');
   } catch (error: any) {
     core.setFailed(error.message);
